2a982e5683adf2b2c62dbb0d58cdbf8616398ef3
Vincent Rodley <vindognz@gmail.com>
2026-06-02 15:50:29 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+
+require("dotenv").config();
+
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/vnz-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+app.command("/vnz-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
